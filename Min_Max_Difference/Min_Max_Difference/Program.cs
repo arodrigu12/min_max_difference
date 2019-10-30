f5f73f2c9fae6ed165af5f2a5c72fb224fc25589
Alfredo Rodriguez <arodrigu12@hotmail.com>
2019-10-29 21:22:01 -0400

    First live coding assignment

--- Min_Max_Difference/Min_Max_Difference/Program.cs
+++ Min_Max_Difference/Min_Max_Difference/Program.cs
@@ -6,7 +6,27 @@ namespace Min_Max_Difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] theNums = { 15, 22, 84, 14, 88, 23 };
+
+            int max = -99;
+            int min = 999;
+
+            foreach (int item in theNums)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+                else if (item > max)
+                {
+                    max = item;
+                }
+            }
+
+
+            Console.WriteLine("min = {0} - max = {1} - difference = {2}", min, max, max - min);
+
+            Console.ReadLine();
         }
     }
 }
